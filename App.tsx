e399f2c1c548d951440d16f5cae9ee5a1e0a75aa
Sergio Navarrete <sergio.nava89@gmail.com>
2021-10-31 15:55:25 +0000

    initial commit

--- App.tsx
+++ App.tsx
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Welcome to One Tap Post, where you will be able to post</Text>
+      <Text>to all your social media platforms with one tap</Text>
       <StatusBar style="auto" />
     </View>
   );
