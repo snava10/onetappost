9e623965a36089da345b0b2af9b30b968affa215
Sergio Navarrete <sergio.nava89@gmail.com>
2021-11-04 15:31:15 +0000

    selecting images from gallery, opening camera for photos and videos. Using context to store the user credentials and pass them accross the different screens

---
+++ GlobalContextProvider.tsx
@@ -0,0 +1,21 @@
+import { FacebookLoginResult } from "expo-facebook";
+import { Component } from "react";
+import { FacebookLogin } from "./FacebookProxy";
+import GlobalContext from "./GlobalContext";
+
+export class GlobalContextProvider extends Component {
+  state = {
+    token: null,
+    user: {user:'dummy'},
+  };
+
+  render() {
+    return (
+      <GlobalContext.Provider value={{
+        facebookLogin: {} as FacebookLogin
+      }}>
+        {this.props.children}
+      </GlobalContext.Provider>
+    );
+  }
+}
